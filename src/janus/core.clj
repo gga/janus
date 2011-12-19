d171764bac788e9d60dfdbdeb10bc324da397555
Giles Alexander <giles.alexander@gmail.com>
2011-12-19 18:38:34 +0000

    Verify that a service will check that the status code of a response.

--- src/janus/core.clj
+++ src/janus/core.clj
@@ -1,9 +1,28 @@
 (ns janus.core
-  [:require [clj-http.client :as client]]
+  [:require [clj-http.client :as http]
+   [json-path]]
   [:use midje.sweet
-        clj-http.fake])
+   clj-http.fake])
 
-(defn verify-service [service])
+(unfinished verify-response)
 
-(fact "should verify status codes"
-  (verify-service {}) => nil?)
+;; Necessary to deal with an apparent bug in Midje.
+(declare f)
+(background (f) => 1)
+
+(defn verify-service [service]
+  (let [response (http/get (:url service))]
+    (map #(verify-response response % service) [:status])))
+
+;.;. Any intelligent fool can make things bigger, more complex, and more
+;.;. violent. It takes a touch of genius -- and a lot of courage -- to move
+;.;. in the opposite direction. -- Schumacher
+(with-fake-routes
+  {"http://example.com/" (fn [req] {:status 201, :headers {}, :body "body"})}
+  
+  (fact "should verify status codes"
+    (verify-service ..service..) => (contains "Expected status: 200. Actual status: 201")
+    (provided
+      (verify-response anything :status anything) => "Expected status: 200. Actual status: 201"
+      ..service.. =contains=> {:url "http://example.com/"}
+      ..service.. =contains=> {:status 200})))
