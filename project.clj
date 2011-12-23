8bf61709da29e201d744f464815ed5c8c37afef9
Giles Alexander <giles.alexander@gmail.com>
2011-12-23 12:11:12 +0000

    Beginning work on verifying the response body.
    However, this is probably an abortive start: there is nothing in here
    to verify that the appropriate verification approach is being used
    given the type of document returned.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject janus "1.0.0-SNAPSHOT"
+(defproject janus "0.0.1"
   :description "Consumer-driven contracts, verified both ways."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.1"]
@@ -7,5 +7,5 @@
                  [org.clojure/core.logic "0.6.6"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
                      [clj-http-fake "0.2.3"]
-                     [midje "1.3.0"]
+                     [midje "1.3.1"]
                      [lein-midje "1.0.4"]])
--- src/janus/core.clj
+++ src/janus/core.clj
@@ -2,27 +2,55 @@
   [:require [clj-http.client :as http]
    [json-path]]
   [:use midje.sweet
-   clj-http.fake])
+   clj-http.fake
+   [clojure.data.json :only [read-json]]])
 
-(unfinished verify-response)
+(unfinished )
 
-;; Necessary to deal with an apparent bug in Midje.
-(declare f)
-(background (f) => 1)
+(defn verify-response-body [body clause])
+
+(fact "should verify that the body matches")
+
+(defn verify-response [response field validation]
+  (if (not= (get response field) (get validation field))
+    (str "Expected " (name field) ": " (get validation field) ". "
+         "Actual "  (name field) ": " (get response field))))
+
+(fact ""
+  (verify-response ..resp.. :status ..validation..) => nil?
+  (provided
+    ..resp.. =contains=> {:status 200}
+    ..validation.. =contains=> {:status 200})
+  (verify-response ..resp.. :status ..validation..) => "Expected status: 200. Actual status: 201"
+  (provided
+    ..resp.. =contains=> {:status 201}
+    ..validation.. =contains=> {:status 200}))
 
 (defn verify-service [service]
-  (let [response (http/get (:url service))]
-    (map #(verify-response response % service) [:status])))
+  (let [response (http/request {:method (:method service), :url (:url service)})
+        body (read-json (:body response))]
+    (filter #(not (nil? %)) (concat
+                             (map #(verify-response response % service) [:status])
+                             (map #(verify-response-body body %) (:clauses service))))))
 
-;.;. Any intelligent fool can make things bigger, more complex, and more
-;.;. violent. It takes a touch of genius -- and a lot of courage -- to move
-;.;. in the opposite direction. -- Schumacher
 (with-fake-routes
-  {"http://example.com/" (fn [req] {:status 201, :headers {}, :body "body"})}
+  {"http://example.com/" (fn [req] {:status 201,
+                                    :headers {},
+                                    :body (str "\"body via " (name (:request-method req)) "\"")})}
   
   (fact "should verify status codes"
     (verify-service ..service..) => (contains "Expected status: 200. Actual status: 201")
     (provided
-      (verify-response anything :status anything) => "Expected status: 200. Actual status: 201"
+      ..service.. =contains=> {:method :get}
       ..service.. =contains=> {:url "http://example.com/"}
-      ..service.. =contains=> {:status 200})))
+      ..service.. =contains=> {:status 200}
+      ..service.. =contains=> {:clauses []}))
+
+  (fact "should issue the request as required by the contract"
+    (verify-service ..service..) => (contains "Body expected to match #\"post\"")
+    (provided
+      (verify-response-body "body via post" anything) => "Body expected to match #\"post\""
+      ..service.. =contains=> {:method :post}
+      ..service.. =contains=> {:url "http://example.com/"}
+      ..service.. =contains=> {:status 201}
+      ..service.. =contains=> {:clauses [{:path "$", :matching #"post"}]})))
