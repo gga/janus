1441cba3eb01b9379c61de5cdfdb27929508cc64
Giles Alexander <giles.alexander@gmail.com>
2012-03-06 22:32:59 +0530

    Load the entirety of each support file, and provide a simple function to run sub-commands.

--- src/janus/support.clj
+++ src/janus/support.clj
@@ -6,12 +6,17 @@
                       (run []
                         (code)))))
 
+(defn system [cmd]
+  (let [proc (.exec (Runtime/getRuntime) cmd)]
+    (.waitFor proc)))
+
 (defn- load-support [support-file]
-  (let [support-ns (create-ns 'support-ns)]
+  (let [support-ns (create-ns 'support-ns)
+        support-text (slurp (.getPath support-file))]
     (binding [*ns* support-ns]
       (eval '(clojure.core/refer 'clojure.core))
       (eval '(refer 'janus.support))
-      (eval (read-string (slurp (.getPath support-file)))))))
+      (load-string support-text))))
 
 (defn environment []
   (let [support-files (file-seq (clojure.java.io/file "." "contracts" "support"))]
